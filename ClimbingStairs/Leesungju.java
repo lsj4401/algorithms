751d712cc3ed47c3ff26c756fedafc91e637d2bd
Leesungju <lsj4401@naver.com>
2016-01-07 15:02:03 +0900

    코드 정리
    불필요한 로직들 제거

--- ClimbingStairs/Leesungju.java
+++ ClimbingStairs/Leesungju.java
@@ -1,20 +1,15 @@
 public class Solution {
-    int[] dp;
-    public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n < 0) return 0;
-        dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        return s(n);
-    }
-    
-    private int s(int n) {
-        if (dp[n] != -1) return dp[n];
-        dp[n] = s(n -1) + s(n -2);
-        
-        return dp[n];
-    }
+  int[] dp;
+  public int climbStairs(int n) {
+    if (n < 0) return 0;
+    dp = new int[Math.max(n + 1, 3)];
+    dp[1] = 1; 
+    dp[2] = 2;
+    return s(n);
+  }
+  
+  private int s(int n) {
+    if (dp[n] != 0) return dp[n];
+    return dp[n] = s(n - 1) + s(n - 2);
+  }
 }
