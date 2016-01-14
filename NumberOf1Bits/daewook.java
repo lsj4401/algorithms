241b874d0ecdc6d9358089a70d01db270f2cf3ac
singun <daeuky1@gmail.com>
2016-01-14 22:07:20 +0900

    이번에는 타임아웃

--- NumberOf1Bits/daewook.java
+++ NumberOf1Bits/daewook.java
@@ -1,16 +1,12 @@
 public int hammingWeight(int n) {
     int res = 0;
-
-    while (n > 0) {
-        if (masking(n,1) % 2 == 1) {
+    
+    while (n != 0) {
+        if ((n & 1) == 1) {
             res++;
         }
         n = n>>1;
     }
 
     return res;
-}
-
-private int masking (int input, int mask) {
-    return input&mask;
 }
