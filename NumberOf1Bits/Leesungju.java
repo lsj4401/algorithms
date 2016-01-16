e8705784ff5f34f5c002af75aa39a12b23789ba4
Leesungju <lsj4401@naver.com>
2016-01-17 01:40:29 +0900

    Update Leesungju.java

--- NumberOf1Bits/Leesungju.java
+++ NumberOf1Bits/Leesungju.java
@@ -1,1 +1,10 @@
-
+public class Solution {
+  public int hammingWeight(int n) {
+    int mask = 1;
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+      count += (n >> i & mask) == 0 ? 0 : 1;
+    }
+    return count;
+  }
+}
