15948d7cd2822a8d061c4100d9e4e1beefcfedfb
singun <daeuky1@gmail.com>
2016-01-16 14:56:22 +0900

    문제별 주석 추가

--- LinkedListCycle/daewook.java
+++ LinkedListCycle/daewook.java
@@ -1,3 +1,8 @@
+// Linked List Cycle
+// Given a linked list, determine if it has a cycle in it.
+// Follow up:
+// Can you solve it without using extra space?
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -9,25 +14,20 @@
  *     }
  * }
  */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head == null)
-            return false;
+public boolean hasCycle(ListNode head) {
+    if (head == null)
+        return false;
 
-        return compareWithNextNode(head, head.next);
-    }
-    
-    private boolean compareWithNextNode(ListNode first, ListNode node) {
-        if (node == null || node.next = null)
-            return false;
-        
-        if (first == node.next)
-            return true;
-        else
-            return compareWithNextNode(first.next, node.next.next);
-        
-    }
+    return compareWithNextNode(head, head.next);
 }
 
-
-////// commit test
+private boolean compareWithNextNode(ListNode first, ListNode node) {
+    if (node == null || node.next = null)
+        return false;
+    
+    if (first == node.next)
+        return true;
+    else
+        return compareWithNextNode(first.next, node.next.next);
+    
+}
