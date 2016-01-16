5b6cb9e6293b08d225157ed9f190b8ddae446595
singun <daeuky1@gmail.com>
2016-01-16 14:13:20 +0900

    굳 잡 붸붸

---
+++ MaximumSubarray/daewook-1loop.java
@@ -0,0 +1,11 @@
+public static int maxSubArray(int[] nums) {
+    int result = nums[0];
+    int sum = nums[0];
+
+    for (int i = 1; i<nums.length; i++) {
+    	sum = Math.max(nums[i], sum + nums[i]);
+    	result = Math.max(sum, result);
+    }
+    
+    return result;
+}
