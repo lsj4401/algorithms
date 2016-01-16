58ab742e0e32bfede030ca83ee062101c2d7ab08
Leesungju <lsj4401@naver.com>
2016-01-17 01:50:03 +0900

    코드 수정
    그렇구나 mask 는 어차피 1이니 필요없고 count 는 걍 더하면 되는구나

--- NumberOf1Bits/Leesungju.java
+++ NumberOf1Bits/Leesungju.java
@@ -1,10 +1,8 @@
-public class Solution {
   public int hammingWeight(int n) {
-    int mask = 1;
     int count = 0;
     for (int i = 0; i < 32; i++) {
-      count += (n >> i & mask) == 0 ? 0 : 1;
+      count += (n >> i & 1);
     }
     return count;
   }
-}
+  
