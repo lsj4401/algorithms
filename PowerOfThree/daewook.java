68156c33c7a3efa577cb0177b432274f291be6a4
singun <daeuky1@gmail.com>
2016-01-24 11:21:23 +0900

    불필요한 것들 모두 제외

--- PowerOfThree/daewook.java
+++ PowerOfThree/daewook.java
@@ -1,24 +1,18 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
         boolean result = true;
-        int input = n;
 
-        if (input <= 0)
+        if (n <= 0)
             return false;
 
-        if (input == 1)
+        if (n == 1)
             return result;
 
-        if(n % 3 == 0) {
-            input = n;
-            while (input > 1) {
-                if(input % 3 == 0)
-                    input = input / 3;
-                else
-                    return false;
-            }
-        } else {
-            return false;
+        while (n > 1) {
+            if(n % 3 == 0)
+                n = n / 3;
+            else
+                return false;
         }
 
         return true;
