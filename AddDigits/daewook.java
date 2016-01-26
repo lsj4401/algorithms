0c2382c9943e94fd09c03de93c13b2d7dbaa0c01
singun <daeuky1@gmail.com>
2016-01-26 14:26:43 +0900

    불필요한 조건문 제거

--- AddDigits/daewook.java
+++ AddDigits/daewook.java
@@ -2,9 +2,6 @@ public class Solution {
     public int addDigits(int num) {
         int sum = 0;
 
-        if (num < 10)
-            return num;
-
         while (num >= 10) {
             sum += num % 10;
             num /= 10;
