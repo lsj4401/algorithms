fb87b87b201455da204d71ba34859c1a954556ec
daewook.laptop <daeuky1@gmail.com>
2016-02-22 23:15:04 +0900

    #18 평균의 최소값 갱신 로직 변경

--- FESTIVAL/daewook.java
+++ FESTIVAL/daewook.java
@@ -1,3 +1,5 @@
+package algospot.festival;
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -9,39 +11,39 @@ public class Main {
 	private static double minFeeAvg = Double.MAX_VALUE;
 
 	public static void main(String[] args) {
-//		try (Scanner sc = new Scanner(
-//				new File("/home/daewook/workspace/springStudy/algospot-festival/src/input1.txt"))) {
-//			int testCases = sc.nextInt();
-//
-//			for (int testIndex = 0; testIndex < testCases; testIndex++) {
-//				rentalbleDays = sc.nextInt();
-//				mainBandsCount = sc.nextInt();
-//
-//				rentFee = new Integer[rentalbleDays];
-//				for (int i = 0; i < rentalbleDays; i++) {
-//					rentFee[i] = sc.nextInt();
-//				}
-//				System.out.println(calculateMinimumFeeAverage());
-//			}
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		
-		Scanner sc = new Scanner(System.in);
-		int testCases = sc.nextInt();
+		try (Scanner sc = new Scanner(
+				new File("/home/daewook/workspace/springStudy/algospot-festival/src/input1.txt"))) {
+			int testCases = sc.nextInt();
 
-		for (int testIndex = 0; testIndex < testCases; testIndex++) {
-			rentalbleDays = sc.nextInt();
-			mainBandsCount = sc.nextInt();
+			for (int testIndex = 0; testIndex < testCases; testIndex++) {
+				rentalbleDays = sc.nextInt();
+				mainBandsCount = sc.nextInt();
 
-			rentFee = new Integer[rentalbleDays];
-			for (int i = 0; i < rentalbleDays; i++) {
-				rentFee[i] = sc.nextInt();
+				rentFee = new Integer[rentalbleDays];
+				for (int i = 0; i < rentalbleDays; i++) {
+					rentFee[i] = sc.nextInt();
+				}
+				System.out.println(calculateMinimumFeeAverage());
 			}
-			System.out.println((double)calculateMinimumFeeAverage());
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		sc.close();
+		
+//		Scanner sc = new Scanner(System.in);
+//		int testCases = sc.nextInt();
+//
+//		for (int testIndex = 0; testIndex < testCases; testIndex++) {
+//			rentalbleDays = sc.nextInt();
+//			mainBandsCount = sc.nextInt();
+//
+//			rentFee = new Integer[rentalbleDays];
+//			for (int i = 0; i < rentalbleDays; i++) {
+//				rentFee[i] = sc.nextInt();
+//			}
+//			System.out.println(calculateMinimumFeeAverage());
+//		}
+//		sc.close();
 		
 	}
 
@@ -49,16 +51,23 @@ public class Main {
 		int sum = recursiveSum(mainBandsCount - 1, mainBandsCount);
 		int cnt = mainBandsCount;
 		minFeeAvg = (double) sum / cnt;
+		System.out.println("init avg " + minFeeAvg);
+		
+		for (int i = 0; i < rentFee.length; i++) {
+			System.out.print(rentFee[i] + " ");
+		}
+		System.out.println();
 
 		for (int i = mainBandsCount; i < rentFee.length; i++) {
 			if (minFeeAvg >= rentFee[i]) {
 				if (rentFee[i] < rentFee[i - cnt]) {
-					minFeeAvg = (double) recursiveSum(i, cnt) / cnt;
+					minFeeAvg = Math.min(minFeeAvg, (double) recursiveSum(i, cnt) / cnt);
 				} else {
 					sum += rentFee[i];
-					minFeeAvg = (double) sum / ++cnt;
+					minFeeAvg = Math.min(minFeeAvg, (double) sum / ++cnt);
 				}
 			}
+			System.out.println(cnt);
 		}
 
 		return minFeeAvg;
