cdfd1c024761255fdb30b7688e6e40afb6c2987e
singun <daeuky1@gmail.com>
2016-03-02 14:48:56 +0900

    #20 타입 리밋

--- PI/daewook.java
+++ PI/daewook.java
@@ -1,86 +1,92 @@
-package test;
+package algospot.pi;
 
-import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int testCase = s.nextInt();
+		String input = new String();
+		
+		for (int i = 0; i < testCase; i++) {
+			input = s.next();
+			init(input);
+			System.out.println(result);
+		}
+		s.close();
+		
+//		try {
+//			Scanner s = new Scanner(new File("c:\\dev\\input2.txt"));
+//			
+//			int testCase = s.nextInt();
+//			String input = new String();
+//			
+//			for (int i = 0; i < testCase; i++) {
+//				input = s.next();
+//				init(input);
+//				System.out.println(result);
+//			}
+//			
+//			s.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}		
+	}
+	
 	public static int[] grade3;
 	public static int[] grade4;
 	public static int[] grade5;
-
-	public static void main(String[] args) {
-//		Scanner s = new Scanner(System.in);
-//		int testCase = s.nextInt();
-//		String input = new String();
-//		
-//		for (int i = 0; i < testCase; i++) {
-//			input = s.next();			
-//			System.out.println(init(input));
-//		}
-//		s.close();
-		
-		try {
-			Scanner s = new Scanner(new File("c:\\dev\\input2.txt"));
-			
-			int testCase = s.nextInt();
-			String input = new String();
-			
-			for (int i = 0; i < testCase; i++) {
-				input = s.next();
-				System.out.println(init(input));
-			}
-			
-			s.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public static int result ;
 	
-	public static int init(String input) {
+	public static void init(String input) {
 		grade3 = new int[input.length()];
 		grade4 = new int[input.length()];
 		grade5 = new int[input.length()];
 		
-		return getMinGrade(input, 0);
+		result = Integer.MAX_VALUE;
+		
+		getMinGrade(input, 0, 0);
 	}
 	
-	public static int getMinGrade (String input, int index) {		
+	
+	public static void getMinGrade (String input, int index, int prevSum) {
 		int blockSize = 3;
 		int blockGrade = 0;
-		int minGrade = Integer.MAX_VALUE;
 		int len = input.length();
 		
+//		System.out.println("input length " + len + ", " + prevSum);
+		
 		if (len < 6 && len > 2) {
-			blockGrade += getBlockGrade(input, index);
-//			System.out.println("final block's grade : " + blockGrade );
-			return blockGrade;
+			blockGrade += getBlockGrade(input, index);			
+			result = Math.min(result, prevSum + blockGrade);
+//			System.out.println("result " + result);
+			
+			return;
 		}
 
 		while (len - blockSize > 2 && blockSize < 6) {
-			blockGrade = 0;
-//			blockGrade += getBlockGrade(Arrays.copyOf(input, blockSize));
-//			blockGrade += getMinGrade(Arrays.copyOfRange(input, blockSize, len));
-			blockGrade += getBlockGrade(input.substring(0, blockSize), index);
-			blockGrade += getMinGrade(input.substring(blockSize, len), index + blockSize);
-			
-			minGrade = Math.min(minGrade, blockGrade);
+//			System.out.println();
+//			System.out.println("block size " + blockSize);
+			blockGrade = getBlockGrade(input.substring(0, blockSize), index);
+//			System.out.println(blockGrade);
+			getMinGrade(input.substring(blockSize, len), index + blockSize, prevSum + blockGrade);
 			blockSize++;
-		}
-		
-		return minGrade;
+		}		
 	}
 	
 	public static int getBlockGrade (String input, int index) {
+		
 		Integer cachedGrade = null;
 		cachedGrade = getCachedGrade(input.length(), index);
 		
-		if (cachedGrade != null)
+		if (cachedGrade != null && cachedGrade != 0)
 			return cachedGrade;
 		
 		int subtract = input.charAt(1) - input.charAt(0);
 		int resultGrade = -1;
 		int prevGrade = -1;
-		int tmpSubtract = 0;
+		int tmpSubtract = 0;		
 		
 		for (int i = 2; i < input.length(); i ++) {
 			prevGrade = resultGrade;
@@ -111,7 +117,7 @@ public class Main {
 		return resultGrade;
 	}
 	
-	public static Integer getCachedGrade(int blockSize, int index) {
+	public static  int getCachedGrade(int blockSize, int index) {
 		Integer cachedGrade = null;
 		
 		if (blockSize == 3)
@@ -120,9 +126,6 @@ public class Main {
 			cachedGrade = grade4[index];
 		else if (blockSize == 5)
 			cachedGrade = grade5[index];
-		
-		if (cachedGrade == 0)
-			cachedGrade = null;
 		
 		return cachedGrade;
 	}
@@ -140,5 +143,4 @@ public class Main {
 			break;			
 		}		
 	}
-	
 }
