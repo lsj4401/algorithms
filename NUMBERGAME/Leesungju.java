86330398ca9a4926e33513adf46306597d49fc46
Leesungju <lsj4401@naver.com>
2016-03-24 08:18:43 +0900

    #22 문제풀이

---
+++ NUMBERGAME/Leesungju.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+  
+  private static int[] input;
+  private static int[][] dp; 
+  private static boolean[][] check;
+  public static void main( String [] arg ) {
+    Scanner sc = new Scanner(System.in);
+    int count = sc.nextInt();
+    for (int i = 0; i < count; i++) {
+      int length = sc.nextInt();
+      input = new int[length];
+      dp = new int[length][length];
+      check = new boolean[length][length];
+      for (int idx = 0; idx < length; idx++) {
+        input[idx] = sc.nextInt();
+      }
+      
+      System.out.println(game(0, input.length - 1, 0, 0, true));
+    }
+    sc.close();
+  }
+  private static int game(int lIdx, int rIdx, int aScore, int bScore, boolean turn) {
+    if (check[lIdx][rIdx]) return dp[lIdx][rIdx];
+    
+    int turnScore = turn ? aScore : bScore;
+    if (rIdx - lIdx == 1) return Math.max(input[rIdx], input[lIdx]) - Math.min(input[rIdx], input[lIdx]);
+    if (rIdx - lIdx == 0) return turnScore + input[lIdx];
+    
+    int woast = Integer.MAX_VALUE;
+    woast = Math.min(woast, game(lIdx + 2, rIdx, aScore, bScore, !turn));
+    woast = Math.min(woast, game(lIdx, rIdx - 2, aScore, bScore, !turn));
+    woast = Math.min(woast, game(lIdx + 1, rIdx, aScore, bScore, !turn) - input[lIdx]);
+    woast = Math.min(woast, game(lIdx, rIdx - 1, aScore, bScore, !turn) - input[rIdx]);
+    
+    check[lIdx][rIdx] = true;
+    
+    return dp[lIdx][rIdx] = turnScore - woast;
+  }
+}
